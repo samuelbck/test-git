a583b2015300a3dce5c70fea70482afe36da148b
samuelbck <samuelbinckly@gmail.com>
2024-09-10 22:29:18 +0200

    Changement méthodes

--- Main.java
+++ Main.java
@@ -1,4 +1,10 @@
-package PACKAGE_NAME;
-
 public class Main {
+
+    public static void main(String[] args) {
+        printHello();
+    }
+
+    public static void printHello() {
+        System.out.println("Hello");
+    }
 }
